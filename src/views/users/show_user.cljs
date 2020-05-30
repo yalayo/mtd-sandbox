e69ced80f4b0041d1ef4f90df223213ef49b0698
Mylab <labs@onmycrowd.com>
2020-05-30 08:39:53 -0700

    Refactoring the show-user view

--- src/views/users/show_user.cljs
+++ src/views/users/show_user.cljs
@@ -3,14 +3,25 @@
             [models.users.core :as model]))
 
 (defn show-user-component [user]
-    [:div
-     [:p (get user :userFullName)]
-     [:p (get user :emailAddress)]
-     [:p (get user :eoriNumber)]
-     [:p (get user :mtdItId)]
-     [:p (get user :nino)]
-     [:p (get user :saUtr)]
-     [:p (get user :vrn)]
-     [:button {:onClick (fn [e]
-                          (.preventDefault e)
-                          (re-frame/dispatch [::model/generate-user]))} "Select"]])
+  [:div
+   [:div.columns.is-mobile.is-multiline
+    [:div.column.is-two-fifths.is-narrow
+     [:p.title.is-4 "Name"]
+     [:p.subtitle.is-6 (get user :userFullName)]]
+    [:div.column
+     [:p.title.is-4 "Email"]
+     [:p.subtitle.is-6 (get user :emailAddress)]]]
+   [:div.columns
+    [:div.column
+     [:p.title.is-4 "National insurance number"]
+     [:p.subtitle.is-6 (get user :nino)]]]
+   [:div.columns
+    [:div.column
+     [:p.title.is-4 "MTD id"]
+     [:p.subtitle.is-6 (get user :mtdItId)]]]
+   [:div.columns.is-vcentered
+    [:div.column
+     [:p.title.is-4 "Sa UTR"]
+     [:p.subtitle.is-6 (get user :saUtr)]]
+    [:div.column.is-one-quarter
+     [:button.button.is-success.is-medium "Select"]]]])
